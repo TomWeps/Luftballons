10f6b289d2fba51fdd3d9386e38e816daf71eeb9
Tomasz Weps <tweps@krolldiscovery.com>
2020-02-02 02:56:07 +0100

    update

--- src/app/ballon/ballon.component.ts
+++ src/app/ballon/ballon.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-ballon',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ballon.component.scss']
 })
 export class BallonComponent implements OnInit {
+
+  @Input()
+  styleClass: string;
 
   constructor() { }
 
