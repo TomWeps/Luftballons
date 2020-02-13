9962e59e6037d9f17ec8bdd011bc5e99955d4dee
Tomasz Weps <tweps@krolldiscovery.com>
2020-02-14 00:09:17 +0100

    cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BallonComponent } from './ballon/ballon.component';
-import { HListComponent } from './h-list/h-list.component';
-import { VListComponent } from './v-list/v-list.component';
 
 @NgModule({
    declarations: [
       AppComponent,
       BallonComponent,
-      HListComponent,
-      VListComponent,
    ],
    imports: [
       BrowserModule
